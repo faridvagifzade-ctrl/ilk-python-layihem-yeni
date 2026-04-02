317bba85f2a6011ac1c177b2cda7c783f8957a2a
Farid Vagifzade <faridvagifzade@gmail.com>
2026-04-02 16:24:27 +0400

    02.04.2026-python dersleri yeni

--- python-sifirdan-zirveye.py
+++ python-sifirdan-zirveye.py
@@ -1,4 +1,4 @@
-#region Dərs 1 Print 
+# region Dərs 1 Print
 
 # print ("Ferid","Vaqifzade", sep=" +-+")
 
@@ -10,10 +10,10 @@ print ("Sag ol")"""
        Her sey yaxsidi?
        """)'''
 
-#endregion 
+# endregion
 
-#region Dərs 2 Dəyişənlər 
-'''x = 4
+# region Dərs 2 Dəyişənlər
+"""x = 4
 y = 5
 z = 6
 
@@ -23,47 +23,47 @@ a=b=c=3
 
 print(a,b,c)
 
-print(x,y,z)'''
+print(x,y,z)"""
 
-'''a = 10
+"""a = 10
 b = 50
 
 print("Evvel:" ,a,b)
 
 a,b = b,a 
 
-print("Sonra :", a,b)'''
+print("Sonra :", a,b)"""
 
-#endregion 
+# endregion
 
-#region Dərs 3 Data Types - Məlumat tipləri
+# region Dərs 3 Data Types - Məlumat tipləri
 
 # integer - int tipi
-'''x = 50
+"""x = 50
 print(x)
 
 # burada type dəyişənin tipini ekrana çıxardır
-print(type(x))'''
+print(type(x))"""
 
 # float
-'''y = 1.09
+"""y = 1.09
 print(y)
-print(type(y))'''
+print(type(y))"""
 
 # string -str
-'''z="soz"
-print(z,":", type(z))'''
+"""z="soz"
+print(z,":", type(z))"""
 
 # boolean - bool
 
-'''d = True
+"""d = True
 print(d,type(d))
-'''
+"""
 
 
-#endregion
+# endregion
 
-#region Dərs 4 Riyazi operatorlar
+# region Dərs 4 Riyazi operatorlar
 
 # // iki dənə bölmə işarəsi ilə ədədləri bölərkən cavabda yalnız tam hissəni göstərəcək
 # % işarəsi ilə bölgü edərkən cavab kimi qalıq hissəni verir.
@@ -83,12 +83,12 @@ print(d,type(d))
 # print(x)
 
 
-#endregion
+# endregion
 
-#region Dərs 5 String
+# region Dərs 5 String
 
 # Burda biz indeksləmə etmişik. Beləki, Azerbaycan sözünün (0 1 2 3 4 5 6 7 8 9 ) indeks
-# içərisində istənilən hərfini ekrana yazırıq              (A z e r b a y c a n ) 
+# içərisində istənilən hərfini ekrana yazırıq              (A z e r b a y c a n )
 
 # olke = "Azerbaycan"
 # print(olke, olke[2])
@@ -118,19 +118,19 @@ print(d,type(d))
 # String-lərin metodları: metodlardan istifadə etmək üçün nöqtədən (.) istifadə edirik.
 # Beləki s dəyişəninin metodlarını cağırmaq üçün s-dən sonra nöqtə qoyuruq.
 
-s="Python"
+# s="Python"
 # print(s.upper())
 # print(s.lower())
 # print(s.strip()) # bu metod sağda və solda olan boşluqları silir.
-# print(s.replace("n", "z")) # bu metod əvəzedici metoddur. 
+# print(s.replace("n", "z")) # bu metod əvəzedici metoddur.
 # print(s.capitalize()) # ilk hərifi böyük yazır ekrana
 # print(s.count("y")) # sözün içində olan hər hansı bir hərfin sayını ekrana çıxardır
 # print(s.find("t")) # bu metod t hərifinin indeksini göstərir.Python sözündə neçənci yerdədir
-# print(s.isdigit()) # bu metod s dəyişənin dəyərinin ədəd olduğunu yoxlayır, ona görə də 
-                     # ekrana False çıxardacaq çünki s-in dəyəri string-dir
+# print(s.isdigit()) # bu metod s dəyişənin dəyərinin ədəd olduğunu yoxlayır, ona görə də
+# ekrana False çıxardacaq çünki s-in dəyəri string-dir
 
 # print(s.isalpha()) # bu metod dəyişənin dəyərinin əlifbada olub olmadığını yoxlayır.
-                   # Nəticə: True
+# Nəticə: True
 
 
 # Bu metodda .format(ad, soyad) o demekdir ki, biz {}{} bu fiqurlu moterizelere
@@ -148,42 +148,167 @@ s="Python"
 # print(f"Salam, {ad} {soyad}")
 
 
-# cumlenin icinde her hansi bir sozu dirnaq icinde yazmaq isteyende \"\" isaresinden 
+# cumlenin icinde her hansi bir sozu dirnaq icinde yazmaq isteyende \"\" isaresinden
 # istifade edirik
 
 # cumle = "Python hemcinin \"Piton\" olaraq da adlandirilir"
 # print(cumle)
 
-# 1:46:57 - Ilkin tip cevirmeleri movzusunda qaldim
+
+# endregion
+
+# region Type Conversion - Tip Cevirmeleri
+
+# interger - float (kesr ededini) interger (tam edede) cevrilmesi
+# a = 1.15
+# print(int(a))
+# print(a)
+
+# b =1.55
+# b= int(b)
+# print(b, type(b))
+
+# endregion
+
+# region Input - Istifadeci girishi
+
+# ad =input("Adinizi daxil edin: " )
+
+# print("Salam, " + ad)
+# print("Salam,", ad)
+
+# Python-da print funksiyasının daxilindəki f hərfi
+# "Formatted String" (Formatlanmış sətir) deməkdir.
+# Buna qısaca f-string deyilir
+# (Dırnağı bir dəfə açırsan, dəyişəni isə birbaşa { }
+# (fiqurlu mötərizə) daxilində yazırsan).
+# print(f"Salam, {ad}")
+
+# print(type(ad))
+
+# eded = input("Eded daxil edin: ")
+
+# print(int(eded) * int(eded))
+# print(int(eded) ** 2)
+
+# eded = int(input("Eded daxil edin: "))
+# print(eded ** 2)
+
+# endregion
+
+# region List (Array) data type - List (Massiv) melumat novu
+# Listler ozunde diger tiplerden olan melumatlari saxlaya bilir
+
+# alinacaqlar_listi = ["Kartof", "Sogan", "Nar", "Pomidor", 10, True, "Nar"]
+
+# print(alinacaqlar_listi)
+# indeksler    0       1       2       3         4
+# meyveler = ["alma", "heyva", "nar", "armud", "portagal"]
+
+# # print(meyveler[:])
+# print(meyveler[2])
+
+# s = "Python"
+# print(s[:])
+
+# burda meyveler listin icincdeki sonuncu portagal elementini kivi elementine deyisirik
+# meyveler[-1] = "kivi"
+
+# print(meyveler[:])
+
+# Append metodu ile biz listlere yeni elementler elave ede bilerik
+# meyveler.append("Kivi")
+# print(meyveler)
+
+# Insert de listlere yeni elementler elave etmek ucundur sadece append-den bir ferqi var
+# Insert-de konkret olaraq hansi yere elave etmeyi bildirmek olar. Append-de ise yalniz
+# listin sonuna elave edir
+
+# meyveler.insert(2, "Yeni Elemenet")
+
+# Remove - metodu elementleri silmek ucundur
+# meyveler.remove("nar")
+
+# Pop-da element silmek ucundur sadece index uzre
+# meyveler.pop(2)
+
+# print(meyveler)
+
+# ededler = [5, 4, -12, 100, 905, 23, 1]
+
+# .Sort metodu elementleri kicikden boyuye dogru siralayir
+# ededler.sort() /*DAHA ETRAFLI PYTHON-QEYDLER WORD FALINDA*/
+
+#  Sorted funksiyasi da elementleri kicikden boyuye dogru siralayir
+# /*DAHA ETRAFLI PYTHON-QEYDLER WORD FALINDA*/
+# ededler = sorted(ededler)
 
 
+#  Sort(reverse=True) metodu elementleri boyukden kiciye dogru siralayir
+# ededler.sort(reverse=True)
+# ededler = sorted(ededler, reverse=True)
+
+# print(*ededler)
+
+# Listlerin birleshdirilmesi
+
+# a = [1,2,3]
+# b = [4,5,6]
+# c = a+b
+# print(*c)
 
 
+# 1-lerden ibaret 5 eded 1 ededini cap edecek
+# a=[1]*5
+# print(*a)
+
+# a = [1,2,3,4,5]
+
+# Clear metodu listin icini temizleyir
+# print(a)
+# a.clear()
+# print(a)
+
+# Count metodu massivin icindeki elementlerin tekrarlanan sayini sayir
+# print(a.count(2))
+
+# Index metodu listin icindeki elementin yerlesdiyi indexi gosterir
+
+# print(a.index(3))
+
+# Reverse metodu listi tersine cevirir
+# a.reverse()
+
+# a = list(reversed(a))
 
 
+# print(a)
+
+# endregion
+
+# region Matrix - 2 olculu siyahilar matrix adlanir
+# Elementler: 0          1
+# list1 = [[1, 2, 3], [4, [5, 6]]]
+# # Elementler:        0     1
+# # Element:0  1  2    0   0  1
+# # Python-da siyahının (list-in) daxilində neçə element olduğunu bilmək üçün
+# # len() (length - uzunluq) funksiyasından istifadə olunur.
+# print("Listin icerisindeki element saylari:", len(list1))
+# print("Sifirinci elementin 2-ci elementi:", list1[0][2])
+# print("Birinci elementin 0-ci elementi:", list1[1][0])
+# print("Birinci elementin icerisindeki 1ci elementin 0-ci elementi:", list1[1][1][0])
+
+# # Listin icinde matrixler
+# list2 = [(1, 2, 3), (4, 5, 6)]
+# print(list2[0][2])
+
+# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# print(matrix[0][2])
+# print(matrix[1][0])
+
+# endregion
+
+# region Tuple melumat novu / 2.35.39 /
 
 
-
-
-
-
-#endregion
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# endregion
